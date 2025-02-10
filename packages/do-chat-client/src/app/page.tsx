104d630a0609a15bdb452d2d74570cebb1bb8fe1
Alwurts <jota780@gmail.com>
2025-02-10 18:51:38 +0100

    Add next js client

---
+++ packages/do-chat-client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="flex h-screen w-screen items-center justify-center">
+			<div className="flex h-full w-full flex-col items-center justify-center bg-background">
+				<h1 className="text-4xl font-bold">DO Chat</h1>
+			</div>
+		</div>
+	);
+}
