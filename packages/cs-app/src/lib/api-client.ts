7412a0c9b0c3a722bf32539ec06c41fde85ab3d4
Alwurts <jota780@gmail.com>
2025-02-27 22:10:51 +0100

    Add more loggin for env variables

--- packages/cs-app/src/lib/api-client.ts
+++ packages/cs-app/src/lib/api-client.ts
@@ -7,8 +7,11 @@ const API_HOST =
 	process.env.NEXT_PUBLIC_DO_CHAT_API_HOST!;
 
 console.log({
-	reason: "API_HOST",
-	API_HOST,
+	reason: "ENV api-client",
+	NEXT_PUBLIC_DO_CHAT_API_HOST: process.env.NEXT_PUBLIC_DO_CHAT_API_HOST,
+	BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+	NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+	BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
 });
 
 export const client = hc<AppType>(API_HOST, {
