29c67decdba33fb2f05744b668b4c092cd2ec3aa
Alwurts <jota780@gmail.com>
2025-03-04 22:06:27 +0100

    Run lint

--- packages/cs-app/src/lib/auth-client.ts
+++ packages/cs-app/src/lib/auth-client.ts
@@ -1,6 +1,6 @@
+import { organizationPermissions } from "@/cs-shared";
 import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import { organizationPermissions } from "@/cs-shared";
 export const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_URL, // the base url of your auth server
 	plugins: [
