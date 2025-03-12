68110b7cef1c1c9549b15da2af755ab50e3dc289
Alwurts <jota780@gmail.com>
2025-03-12 18:50:38 +0100

    MIgration for threadId schema change

--- packages/cs-api/src/durable-objects/chat-room/db/migrations/0000_wise_mysterio.sql
+++ packages/cs-api/src/durable-objects/chat-room/db/migrations/0000_wise_mysterio.sql
@@ -5,6 +5,7 @@ CREATE TABLE `chat_message` (
 	`member_id` text NOT NULL,
 	`created_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
 	`metadata` text NOT NULL,
+	`thread_id` integer,
 	FOREIGN KEY (`member_id`) REFERENCES `chat_room_member`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
