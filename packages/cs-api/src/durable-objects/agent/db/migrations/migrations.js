dbae324d9fd0a03d00aa6bff144370600f117866
Alwurts <jota780@gmail.com>
2025-03-23 15:24:10 +0100

    Add updated schema for agents

--- packages/cs-api/src/durable-objects/agent/db/migrations/migrations.js
+++ packages/cs-api/src/durable-objects/agent/db/migrations/migrations.js
@@ -1,4 +1,4 @@
-import m0000 from "./0000_flashy_pepper_potts.sql";
+import m0000 from "./0000_giant_ser_duncan.sql";
 import journal from "./meta/_journal.json";
 
 export default {
--- packages/cs-shared/src/db/schema/agent.ts
+++ packages/cs-shared/src/db/schema/agent.ts
@@ -1,13 +1,26 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 import { organization } from "./auth";
+import type {
+	EmojiUsage,
+	Verbosity,
+	LanguageStyle,
+	Tone,
+} from "../../types/agent";
 
 export const agent = sqliteTable("agent", {
 	id: text("id").primaryKey(), // Same as the DO ID
+	email: text("email").notNull().unique(),
+	// Identity
 	name: text("name").notNull(),
 	image: text("image").notNull(),
-	systemPrompt: text("system_prompt").notNull(),
-	// TODO: Add email here and on the DO
+	description: text("description").notNull(),
+	// Personality
+	tone: text("tone").$type<Tone>().notNull(),
+	verbosity: text("verbosity").$type<Verbosity>().notNull(),
+	emojiUsage: text("emoji_usage").$type<EmojiUsage>().notNull(),
+	languageStyle: text("language_style").$type<LanguageStyle>().notNull(),
+	// Metadata
 	organizationId: text("organization_id")
 		.notNull()
 		.references(() => organization.id),
